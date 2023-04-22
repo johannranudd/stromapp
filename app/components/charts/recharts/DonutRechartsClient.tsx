d71a6e03ff80097d9481dad96bc8024ee3b3d170
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-04-22 22:25:40 +0200

    cleanup in DonutClinet.tsx and next.js updated to current version

--- app/components/charts/recharts/DonutRechartsClient.tsx
+++ app/components/charts/recharts/DonutRechartsClient.tsx
@@ -1,28 +1,15 @@
 "use client";
 import { getElSupportPercentage } from "@/app/utils/generics";
-// import { Group } from "@visx/group";
-// import { Text } from "@visx/text";
 import {
   PieChart,
   ResponsiveContainer,
   Pie,
   Tooltip,
   Cell,
-  Label,
   Text,
 } from "recharts";
 import { useGlobalContext } from "@/app/context/context";
-import { useTheme } from "next-themes";
 import { useState, useEffect } from "react";
-
-// const data = [
-//   { name: "Group A", value: 2400 },
-//   { name: "Group B", value: 4567 },
-//   { name: "Group C", value: 1398 },
-//   { name: "Group D", value: 9800 },
-//   { name: "Group E", value: 3908 },
-//   { name: "Group F", value: 4800 },
-// ];
 
 const COLORS = [
   "#ce93d8",
@@ -35,7 +22,6 @@ const COLORS = [
 
 export default function DonutRechartsServer(dataFromAPI: any) {
   const { windowWidth } = useGlobalContext();
-  const { theme } = useTheme();
   const now = new Date();
   const options: any = {
     timeZone: "Europe/Oslo",
@@ -60,7 +46,6 @@ export default function DonutRechartsServer(dataFromAPI: any) {
   } = dataFromAPI[0];
 
   const [width, setWidth] = useState(windowWidth / 1.3);
-  //   const half = width / 2;
   useEffect(() => {
     if (windowWidth >= 400) {
       setWidth(300);
@@ -77,14 +62,13 @@ export default function DonutRechartsServer(dataFromAPI: any) {
         zIndex: 1,
         margin: windowWidth >= 400 ? "0 0 0 auto" : "0 auto 0 auto",
       }}
-      //   className="relative bg-red-500 xxs:bg-blue-500"
       className="relative "
     >
       {dailyPriceArray?.map((priceInOre: any, index: number) => {
         let hour = `${index}`;
         index < 10 ? (hour = `0${index}`) : `${index}`;
 
-        const priceInOreAndHour = { priceInOre, hour };
+        const priceInOreAndHour: any = { priceInOre, hour };
 
         const oneHourAhead = Number(priceInOreAndHour.hour) + 1;
         const oneHourAheadToString = `${
@@ -109,65 +93,13 @@ export default function DonutRechartsServer(dataFromAPI: any) {
           ];
 
           return (
-            <>
-              <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
-                <h2 className="text-center" style={{ fontSize: width / 10 }}>
-                  <strong>{priceInOreAndHour.priceInOre}</strong> Øre
-                </h2>
-                <p className="text-center">{timeFrameForCurrentPrice}</p>
-              </div>
-              <ResponsiveContainer>
-                <PieChart>
-                  <Pie
-                    dataKey="value"
-                    data={data}
-                    innerRadius={width / 2.3}
-                    // innerRadius={125}
-                    outerRadius={width / 1.9}
-                    // outerRadius={150}
-                    fill="#82ca9d"
-                    label={({
-                      cx,
-                      cy,
-                      midAngle,
-                      innerRadius,
-                      outerRadius,
-                      percent,
-                      index,
-                    }) => {
-                      const radius =
-                        innerRadius + (outerRadius - innerRadius) * 0.5;
-                      const x =
-                        cx + radius * Math.cos(-midAngle * (Math.PI / 180));
-                      const y =
-                        cy + radius * Math.sin(-midAngle * (Math.PI / 180));
-                      return (
-                        <Text
-                          x={index === 0 ? x + 50 : x - 50}
-                          y={index === 0 ? y + 40 : y - 20}
-                          textAnchor="middle"
-                          alignmentBaseline="middle"
-                          fill={COLORS[index % COLORS.length]}
-                          fontSize={16}
-                          fontWeight={600}
-                        >
-                          {data[index].name}
-                        </Text>
-                      );
-                    }}
-                    labelLine={false}
-                  >
-                    {data.map((entry, index) => (
-                      <Cell
-                        key={`cell-${index}`}
-                        fill={COLORS[index % COLORS.length]}
-                      />
-                    ))}
-                  </Pie>
-                  <Tooltip />
-                </PieChart>
-              </ResponsiveContainer>
-            </>
+            <Donut
+              key={_id}
+              priceInOreAndHour={priceInOreAndHour}
+              data={data}
+              width={width}
+              timeFrameForCurrentPrice={timeFrameForCurrentPrice}
+            />
           );
         }
       })}
@@ -175,62 +107,66 @@ export default function DonutRechartsServer(dataFromAPI: any) {
   );
 }
 
-{
-  /* <Group>
-    <Text
-                    x={-width / 9}
-                    y={-70}
-                    fontSize={width / 10}
-                    textAnchor="middle"
-                    className="font-bold"
-                    fill={theme === "light" ? "#242431" : "#fbf5f5"}
-                  >
-                    {priceInOreAndHour.priceInOre}
-                  </Text>
-                  <Text
-                    x={width / 9}
-                    y={-70}
-                    fontSize={width / 10}
-                    textAnchor="middle"
-                    className="duration-300"
-                    fill={theme === "light" ? "#242431" : "#fbf5f5"}
-                  >
-                    Øre
-                  </Text>
-                </Group>
+function Donut({
+  data,
+  priceInOreAndHour,
+  timeFrameForCurrentPrice,
+  width,
+}: any) {
+  return (
+    <>
+      <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+        <h2 className="text-center" style={{ fontSize: width / 10 }}>
+          <strong>{priceInOreAndHour.priceInOre}</strong> Øre
+        </h2>
+        <p className="text-center">{timeFrameForCurrentPrice}</p>
+      </div>
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie
+            dataKey="value"
+            data={data}
+            innerRadius={width / 2.3}
+            outerRadius={width / 1.9}
+            fill="#82ca9d"
+            label={({
+              cx,
+              cy,
+              midAngle,
+              innerRadius,
+              outerRadius,
+              percent,
+              index,
+            }) => {
+              const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+              const x = cx + radius * Math.cos(-midAngle * (Math.PI / 180));
+              const y = cy + radius * Math.sin(-midAngle * (Math.PI / 180));
+              return (
                 <Text
-                  // x={width / 9}
-                  y={-30}
-                  fontSize={width / 15}
+                  x={index === 0 ? x + 50 : x - 50}
+                  y={index === 0 ? y + 40 : y - 20}
                   textAnchor="middle"
-                  className="duration-300"
-                  fill={theme === "light" ? "#242431" : "#fbf5f5"}
+                  alignmentBaseline="middle"
+                  fill={COLORS[index % COLORS.length]}
+                  fontSize={16}
+                  fontWeight={600}
                 >
-                  {timeFrameForCurrentPrice}
+                  {data[index].name}
                 </Text>
-                {data.map((item: any, index: number) => {
-                  return (
-                    <>
-                      <Text
-                        x={0}
-                        y={index === 0 ? 40 : 10}
-                        fontSize={width / 18}
-                        textAnchor="end"
-                        fill={item.color}
-                        className="duration-300"
-                      >
-                        {data && data[index].name}
-                      </Text>
-                      <Text
-                        x={50}
-                        y={index === 0 ? 40 : 10}
-                        fontSize={width / 18}
-                        fill={item.color}
-                        className="duration-300 "
-                      >
-                        {`${item.value}%`}
-                      </Text>
-                    </>
-                  );
-                })} */
+              );
+            }}
+            labelLine={false}
+          >
+            {data.map((entry: any, index: number) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+        </PieChart>
+      </ResponsiveContainer>
+    </>
+  );
 }
