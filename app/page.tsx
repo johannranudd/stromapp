52d39fbf40639e9f6b6c18796f64e160e72f3be0
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-04-22 05:19:50 +0200

    recharts donut works. push to main

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import DonutRechartsServer from "./components/charts/recharts/DonutRechartsServer";
 import XYServer from "./components/charts/recharts/XYServer";
 import DonutChart from "./components/charts/visx/DonutChart";
 
 export default function Home() {
   return (
     <main className="w-[95%] max-w-screen-xl mx-auto">
-      <DonutChart />
+      {/* <DonutChart /> */}
+      <DonutRechartsServer />
       <div className="my-12 py-12">
         <XYServer />
       </div>
