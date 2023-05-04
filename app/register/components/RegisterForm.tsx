beb35b4c3c67141bdf71a564de29bc50d09be575
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-05-04 20:51:32 +0200

    adds styling register form

--- app/register/components/RegisterForm.tsx
+++ app/register/components/RegisterForm.tsx
@@ -54,77 +54,90 @@ export default function RegisterForm() {
   return (
     <>
       {errors.length > 0 && (
-        <div className="absolute top-[25%] left-[50%] translate-x-[-50%]  w-full max-w-[400px] z-50 flex flex-col items-center py-6 bg-red-500">
-          <p>{errors}</p>
+        <div className="absolute top-[5%] left-[50%] translate-x-[-50%] w-[95%] max-w-[400px] z-50 flex flex-col items-center py-6 bg-red-500">
+          {errors}
         </div>
       )}
       <div className="relative h-[calc(100vh-4rem)]">
         <form
           onSubmit={handleSubmit}
-          className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-100%] flex flex-col justify-center items-center w-[95%] max-w-[400px] border"
+          className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[95%] max-w-[400px] flex flex-col justify-center items-center space-y-6 py-6"
         >
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={name}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setName(e.target.value)
-            }
-            required
-          />
+          <h1 className="text-2xl">Register</h1>
+          <div className="flex flex-col w-full space-y-3">
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setName(e.target.value)
+              }
+              required
+            />
+          </div>
+          <div className="flex flex-col w-full space-y-3">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setEmail(e.target.value)
+              }
+              required
+            />
+          </div>
+          <div className="flex flex-col w-full space-y-3">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPassword(e.target.value)
+              }
+              required
+            />
+          </div>
+          <div className="flex flex-col w-full space-y-3">
+            <label htmlFor="address">Address:</label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              value={address}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setAddress(e.target.value)
+              }
+              required
+            />
+          </div>
+          <div className="flex flex-col w-full space-y-3">
+            <label htmlFor="phoneNumber">phone Number:</label>
+            <input
+              type="text"
+              id="phoneNumber"
+              name="phoneNumber"
+              value={phoneNumber}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPhoneNumber(e.target.value)
+              }
+              required
+            />
+          </div>
 
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setEmail(e.target.value)
-            }
-            required
-          />
-
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setPassword(e.target.value)
-            }
-            required
-          />
-
-          <label htmlFor="address">Address:</label>
-          <input
-            type="text"
-            id="address"
-            name="address"
-            value={address}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setAddress(e.target.value)
-            }
-            required
-          />
-          <label htmlFor="phoneNumber">phone Number:</label>
-          <input
-            type="text"
-            id="phoneNumber"
-            name="phoneNumber"
-            value={phoneNumber}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setPhoneNumber(e.target.value)
-            }
-            required
-          />
-
-          <button type="submit">Register</button>
+          <button type="submit" className="btnCtaWide">
+            Register
+          </button>
           <p>
-            already have a user? <Link href={"/login"}>Login&gt;&gt;</Link>
+            already have a user?{" "}
+            <Link href={"/login"} className="text-fourthClr">
+              Login&gt;&gt;
+            </Link>
           </p>
         </form>
       </div>
