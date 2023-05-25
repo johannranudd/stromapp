4a4c6684a10c3af36702690a2bc81618b701e95c
johannranudd <33978097+johannranudd@users.noreply.github.com>
2023-05-25 22:01:10 +0200

    api works from 05/25. merge and deploy

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { use } from "react";
 import BtnCta from "./components/ui/BtnCta";
 import HeroVector from "../assets/images/verctor-hero.svg";
 import Image from "next/image";
+// deploy 2, api works from 05/25
 
 export default function Home() {
   const currentDate: Date = new Date();
